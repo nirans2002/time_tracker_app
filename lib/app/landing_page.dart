28507938e7ab01def3df923c4bf9eff3718155b4
Niran S Narayanan <nirans2002@gmail.com>
2021-08-18 23:24:25 +0530

    video 158

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -40,15 +40,29 @@ class _LandingPageState extends State<LandingPage> {
 
   @override
   Widget build(BuildContext context) {
-    if (_user == null) {
-      return SignInPage(
-        auth: widget.auth,
-        onSignIn: _updateUser,
-      );
-    }
-    return HomePage(
-      auth: widget.auth,
-      onSignOut: () => _updateUser(null),
-    );
+    return StreamBuilder(
+        stream: widget.auth.onAuthStateChanged,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            User user = snapshot.data;
+
+            if (user == null) {
+              return SignInPage(
+                auth: widget.auth,
+                onSignIn: _updateUser,
+              );
+            }
+            return HomePage(
+              auth: widget.auth,
+              onSignOut: () => _updateUser(null),
+            );
+          } else {
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        });
   }
 }
