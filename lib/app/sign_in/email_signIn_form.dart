a7c82205a8eb8d57d0d3398b75c920d80e6fd38f
Niran S Narayanan <nirans2002@gmail.com>
2021-08-23 20:44:15 +0530

    text controller , 190

--- lib/app/sign_in/email_signIn_form.dart
+++ lib/app/sign_in/email_signIn_form.dart
@@ -2,24 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:time_tracker_app/components/form_submit_button.dart';
 
 class EmailSignInForm extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _submit() {
+    print(
+        'email : ${_emailController.text}, password : ${_passwordController.text}');
+  }
+
   List<Widget> _buidChildren() {
     return [
       TextField(
+        controller: _emailController,
         decoration: InputDecoration(
           labelText: "Email",
           hintText: 'johnappleseed@example.com',
         ),
+        // onChanged: (value) => email = value,
       ),
       SizedBox(height: 8.0),
       TextField(
+        controller: _passwordController,
+
         decoration: InputDecoration(
           labelText: "Password",
         ),
         obscureText: true,
+        // onChanged: (value) => password = value,
       ),
       SizedBox(height: 8.0),
       FormSubmitButton(
-        onPressed: () {},
+        onPressed: _submit,
         text: "Sign in",
       ),
       SizedBox(height: 8.0),
