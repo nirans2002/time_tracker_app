78a5231dc14a9252d06f4d3bf4538b555fbc83c7
Niran S Narayanan <nirans2002@gmail.com>
2021-08-23 12:43:04 +0530

    email,psswrd input , 187

--- lib/app/sign_in/email_signIn_form.dart
+++ lib/app/sign_in/email_signIn_form.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 
 class EmailSignInForm extends StatelessWidget {
-  List<Widget> _buidChildren() {}
+  List<Widget> _buidChildren() {
+    return [
+      TextField(
+        decoration: InputDecoration(
+          labelText: "Email",
+          hintText: 'johnappleseed@example.com',
+        ),
+      ),
+      TextField(
+        decoration: InputDecoration(
+          labelText: "Password",
+        ),
+        obscureText: true,
+      )
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: _buidChildren(),
+      ),
     );
   }
 }
