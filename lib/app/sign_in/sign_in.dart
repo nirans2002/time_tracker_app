0c61737950fcb8d20ef34bbdc990d7d7240a6d13
Niran S Narayanan <nirans2002@gmail.com>
2021-08-23 23:16:02 +0530

    px overflow in sign in page fixed-line60

--- lib/app/sign_in/sign_in.dart
+++ lib/app/sign_in/sign_in.dart
@@ -57,7 +57,7 @@ class SignInPage extends StatelessWidget {
         title: Text("Time Tracker  App"),
         elevation: 10.0,
       ),
-      body: _buildContent(context),
+      body: SingleChildScrollView(child: _buildContent(context)),
       backgroundColor: Colors.grey[50],
     );
   }
