96200c8892a124b73df91e314f1389b0b1e32228
Niran S Narayanan <nirans2002@gmail.com>
2021-08-23 22:51:52 +0530

    str validation, 202.

--- lib/app/sign_in/email_signIn_form.dart
+++ lib/app/sign_in/email_signIn_form.dart
@@ -24,10 +24,13 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
   bool _submitted = false;
 
   void _submit() async {
+    print('submit called')
     setState(() {
       _submitted = true;
     });
     try {
+      // temporarily
+      await Future.delayed(Duration(seconds: 3));
       if (_formType == EmailSignInFormType.signIn) {
         await widget.auth.signInWithEmailAndPassword(_email, _password);
       } else {
