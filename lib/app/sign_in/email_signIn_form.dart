3fdad02d150e6c260def754a94337dde8b444332
Niran S Narayanan <nirans2002@gmail.com>
2021-08-23 12:51:23 +0530

    sign in btn , 188

--- lib/app/sign_in/email_signIn_form.dart
+++ lib/app/sign_in/email_signIn_form.dart
@@ -9,12 +9,23 @@ class EmailSignInForm extends StatelessWidget {
           hintText: 'johnappleseed@example.com',
         ),
       ),
+      SizedBox(height: 8.0),
       TextField(
         decoration: InputDecoration(
           labelText: "Password",
         ),
         obscureText: true,
-      )
+      ),
+      SizedBox(height: 8.0),
+      ElevatedButton(
+        onPressed: () {},
+        child: Text("Sign in"),
+      ),
+      SizedBox(height: 8.0),
+      TextButton(
+        onPressed: () {},
+        child: Text('Create an account now'),
+      ),
     ];
   }
 
@@ -23,6 +34,7 @@ class EmailSignInForm extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisSize: MainAxisSize.min,
         children: _buidChildren(),
       ),
