9d3ca0faac85d818f10c6ef6373a52ac3b18e816
Niran S Narayanan <nirans2002@gmail.com>
2021-08-23 23:02:11 +0530

    str validation, 204

--- lib/app/sign_in/email_signIn_form.dart
+++ lib/app/sign_in/email_signIn_form.dart
@@ -46,7 +46,10 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
   }
 
   void _emailEditingComplete() {
-    FocusScope.of(context).requestFocus(_passwordFocusNode);
+    final newFocus = widget.emailValidator.isValid(_email)
+        ? _passwordFocusNode
+        : _emailFocusNode;
+    FocusScope.of(context).requestFocus(newFocus);
   }
 
   void _toggleFormType() {
